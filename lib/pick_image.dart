be39de9e0eb4e7f3d0fb84094ad90cd4b5764097
yamish01 <yamishthakur@gmail.com>
2024-04-02 12:57:30 +0530

    image update

--- lib/pick_image.dart
+++ lib/pick_image.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'dart:io' as io;
 
 class PickImageScreen extends StatefulWidget {
   const PickImageScreen({super.key});
@@ -12,15 +17,24 @@ class PickImageScreen extends StatefulWidget {
 }
 
 class _PickImageScreenState extends State<PickImageScreen> {
-  void pickImage() {}
+  final ImagePicker picker = ImagePicker();
+  XFile? xfile = null;
+
+  void pickImage() async {
+    print("image pick");
+    xfile = await picker.pickImage(source: ImageSource.gallery);
+    print("xfile $xfile");
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Upload image")),
       body: Column(
         children: [
-          GestureDetector(
-              onTap: () async {
+          ElevatedButton(
+              onPressed: () async {
                 if (Platform.isAndroid) {
                   final androidInfo = await DeviceInfoPlugin().androidInfo;
                   if (androidInfo.version.sdkInt <= 32) {
@@ -58,11 +72,42 @@ class _PickImageScreenState extends State<PickImageScreen> {
                   }
                 }
               },
-              child: Text("upload image")),
-          Image.network(
-              width: 200,
-              height: 200,
-              "https://firebasestorage.googleapis.com/v0/b/first-project-cd333.appspot.com/o/insta%20(2).png?alt=media&token=e9490703-1ee7-4214-97ed-40f98ee9968d"),
+              child: Text("select image")),
+          xfile != null
+              ? Image.file(File(xfile?.path ?? ""))
+              : CachedNetworkImage(
+                  imageUrl:
+                      "https://firebasestorage.googleapis.com/v0/b/first-project-cd333.appspot.com/o/insta%20(2).png?alt=media&token=e9490703-1ee7-4214-97ed-40f98ee9968d",
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+
+          // Image.network(
+          //     width: 200,
+          //     height: 200,
+          // "https://firebasestorage.googleapis.com/v0/b/first-project-cd333.appspot.com/o/insta%20(2).png?alt=media&token=e9490703-1ee7-4214-97ed-40f98ee9968d"),
+          ElevatedButton(
+              onPressed: () async {
+                File file = File(xfile?.path ?? "");
+                Reference ref = FirebaseStorage.instance
+                    .ref()
+                    .child('flutter-tests')
+                    .child('/some-image.jpg');
+                final MetaData = SettableMetadata(
+                  contentType: 'image/jpg',
+                  customMetadata: {'picked-file-path': xfile?.path ?? ""},
+                );
+                UploadTask uploadTask;
+                if (kIsWeb) {
+                  uploadTask = ref.putData(await file.readAsBytes(), MetaData);
+                  uploadTask.then((p0) => print("p0 $p0"));
+                } else {
+                  uploadTask = ref.putFile(io.File(file.path), MetaData);
+                  print("ref.getDownLoadURL(); ${await ref.getDownloadURL()}");
+                  uploadTask.then(((p0) => null));
+                }
+              },
+              child: Text("upload Image"))
         ],
       ),
     );
